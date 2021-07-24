74c471a60c404f7d1c25349a4b6e826e982e7a66
Pablo Junio Souza Santos <84691296+pablowan@users.noreply.github.com>
2021-07-24 14:51:25 -0300

    Commited

---
+++ servidor.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const app = express();
+const database = require("./database.json");
+
+app.use(express.json());
+
+app.get("/users", function(req, res) {
+  res.json(database);
+});
+
+app.get("/users/:id", function(req, res) {
+  const { id } = req.params;
+  const users = database.find(cli => cli.id == id);
+
+  if (!users) return res.status(204).json();
+
+  res.json(users);
+});
+
+app.post("/users", function(req, res) {
+  const { name, email } = req.body;
+
+  // salvar
+
+  res.json({ name, email });
+});
+
+app.put("/users/:id", function(req, res) {
+  const { id } = req.params;
+  const users = database.find(cli => cli.id == id);
+
+  if (!users) return res.status(204).json();
+
+  const { name , email} = req.body;
+
+  users.name = name;
+  users.email= email;
+
+  res.json(users);
+});
+
+app.delete("/users/:id", function(req, res) {
+  const { id } = req.params;
+  const usersFiltered = database.filter(users => users.id != id);
+
+  res.json(usersFiltered);
+});
+
+app.listen(8080, function() {
+  console.log("Seu servidor se encontra em funcionamento!");
+});
